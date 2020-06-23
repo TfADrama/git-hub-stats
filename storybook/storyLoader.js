9e7f0b95d0e39d625cb0b02718a1b5cac96058cb
Tiago Freitas <tfreitas@impresa.pt>
2020-06-23 20:00:19 +0100

    initial setup

---
+++ storybook/storyLoader.js
@@ -0,0 +1,17 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+	require('../__stories__/index.stories');
+}
+
+const stories = [
+	'../__stories__/index.stories'
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
